cd3f949ce3e0b323244bd75dda971827a3c83da5
TomWhite94 <tomjameswhite94@gmail.com>
2020-09-01 12:26:12 +0100

    start

--- bwf/src/App.js
+++ bwf/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Taskbar from './Taskbar'
 
 function App() {
   return (
     <div className="App">
-      
+      <Taskbar />
+     Hog Nuts
     </div>
   );
 }
---
+++ bwf/src/Taskbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+
+class Taskbar extends Component {
+    render() {
+        return(
+            <div>This will be the taskbar</div>
+        )
+    }
+}
+
+export default Taskbar
