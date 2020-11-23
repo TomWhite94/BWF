4090fce9e4c997ec227c6d788172edcfd5b61aa8
TomWhite94 <tomjameswhite94@gmail.com>
2020-11-23 17:08:01 +0000

    styling and formatting of navbar and homepage

--- bwf/src/App.js
+++ bwf/src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
-import Taskbar from './Taskbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <Taskbar />
-     TEST
+    <div className="App imgbox">
+       <Navbar className="navbar sticky-top App" expand="lg">
+                <Navbar.Brand href="#home"> 
+                    <img
+                        alt=""
+                        src={require("./assets/bwflightning.png")}
+                        width="10%"
+                        height="auto"
+                        className="d-inline-block align-top float"             
+                    />
+                </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                   
+                    </Nav>
+                </Navbar.Collapse>
+          </Navbar>
+        <img 
+            alt=""
+            src={require("./assets/bwf-blog.png")}
+            className="d-inline-block align-top center-fit"
+            width="40%"
+            height="auto"
+        />
+         
     </div>
   );
 }
