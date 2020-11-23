c6010caa851be9b3469bf9de7843d1d197c696a1
TomWhite94 <tomjameswhite94@gmail.com>
2020-11-23 14:43:59 +0000

    colors

--- bwf/src/Taskbar.js
+++ bwf/src/Taskbar.js
@@ -9,7 +9,7 @@ import Button from 'react-bootstrap/Button'
 class Taskbar extends Component {
     render() {
         return(
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
