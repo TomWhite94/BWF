dd87b5db88814a999d02b9fbb4317286532b7f31
TomWhite94 <tomjameswhite94@gmail.com>
2020-11-24 11:50:43 +0000

    scrollspy

--- bwf/src/App.js
+++ bwf/src/App.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-function App() {
-  return (
+
+class App extends Component {
+
+  
+  render() {
+    return (
     <div className="App imgbox">
       {/* Navbar with bwf logo */}
-       <Navbar className="navbar sticky-top App" expand="lg">
+       <Navbar id="navbar-scrollspy" className="navbar sticky-top App" variant="dark" expand="lg">
                 <Navbar.Brand href="#home"> 
                     <img
                         alt=""
                         src={require("./assets/bwflightning.png")}
-                        width="10%"
+                        width="5%"
                         height="auto"
                         className="d-inline-block align-top float"             
-                    />
-                </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                   
+                    />{' '}
+                    <Nav className="mr-auto App">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </Navbar.Brand>
+                
           </Navbar>
           {/* BWF blog post with band header */}
+         
         <img 
             alt=""
             src={require("./assets/bwf-blog.png")}
@@ -33,9 +37,34 @@ function App() {
             width="40%"
             height="auto"
         />
-         
+         <img 
+            alt=""
+            src={require("./assets/bwf-blog.png")}
+            className="d-inline-block align-top center-fit"
+            width="40%"
+            height="auto"
+        />
+         <img 
+            href="#option3"
+            alt=""
+            src={require("./assets/bwf-blog.png")}
+            className="d-inline-block align-top center-fit"
+            width="40%"
+            height="auto"
+        />
+         <img 
+            alt=""
+            src={require("./assets/bwf-blog.png")}
+            className="d-inline-block align-top center-fit"
+            width="40%"
+            height="auto"
+        />
+
     </div>
   );
 }
+}
+
+// Parralax
 
 export default App;
