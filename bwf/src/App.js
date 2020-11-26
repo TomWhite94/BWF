4c9660c1efa20bbe918942d080efc352705cb95c
TomWhite94 <tomjameswhite94@gmail.com>
2020-11-26 11:13:01 +0000

    parallax

--- bwf/src/App.js
+++ bwf/src/App.js
@@ -12,62 +12,64 @@ class App extends Component {
   
   render() {
     return (
-    <div className="App imgbox">
+    <div className="App imgbox  ">
       {/* Navbar with bwf logo */}
-       <Navbar id="navbar-scrollspy" className="navbar App" sticky="top" variant="dark" expand="lg">
+       <Navbar className="navbar App" sticky="top" variant="dark" expand="lg">
                     <Navbar.Brand href="#home">
                    
                     <img
                         alt=""
                         src={require("./assets/bwflightning.png")}
-                        width="150"
-                        height="150"
+                        width="100"
+                        height="100"
                         className="d-inline-block align-top float"             
                     />
                     </Navbar.Brand>
+                    {/* scrolling code */}
                     <Nav className="mr-auto Navbar-text">
-                      <Nav.Link onClick={() => scroll.scrollToTop()}>Scroll To Bottom</Nav.Link>
-                      <Link activeClass="active" className="test2 nav-link" to="test2" spy={true} smooth={true} duration={500} offset={-176} >Link</Link>
-                      <Link activeClass="active" className="test3 nav-link" to="test3" spy={true} smooth={true} duration={500} >Section</Link>
+                      <Nav.Link onClick={() => scroll.scrollToTop()}>Scroll To Top</Nav.Link>
+                      <Link activeClass="active" className="test2 nav-link" to="test1" spy={true} smooth={true} duration={500} offset={-176} >Link</Link>
+                      <Link activeClass="active" className="test3 nav-link" to="test2" spy={true} smooth={true} duration={500} >Section</Link>
                     </Nav>
                 
                 
           </Navbar>
           {/* BWF blog post with band header */}
           {/* <div data-spy="scroll" data-target="#navbar-scrollspy" data-offset="0"> */}
-          <Row><Col><img
-            name="test1"
+          <div className="wrapper">
+         <Row className="section static"><Col><img
             id="section1"
             alt=""
-            src={require("./assets/bwflogo.png")}
+            src={require("./assets/bwflogo.jpg")}
             className="d-inline-block align-top center-fit"
-            width="40%"
+            width="30%"
             height="auto"
         /></Col></Row>
-        <Row><Col><img
-            name="test2"
+        <section className="section2 parallax bg1"></section>
+        <section className="section3 static"><img
+            name="test1"
             id="section2"
             alt=""
             src={require("./assets/bwf-blog.png")}
             className="d-inline-block align-top center-fit"
             width="40%"
             height="auto"
-        /></Col></Row>
-          <Row><Col><img
+        /></section>
+          {/* <Row className="section static"><Col><img
             name="test3"
             id="section3"
             alt=""
             src={require("./assets/bwf-blog.png")}
-            className="d-inline-block align-top center-fit"
+            className="d-inline-block align-top center-fit section static"
             width="40%"
             height="auto"
-        /></Col></Row>
+        /></Col></Row> */}
 
 
           {/* </div> */}
           
-
-    </div>
+        </div>
+    </div>   
   );
 }
 }
